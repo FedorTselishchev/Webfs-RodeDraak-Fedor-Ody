87be620ca767d525c56127f876303e97addc234b
FedorTselishchev <ikbenfedor@gmail.com>
2021-06-17 15:44:50 +0200

    initial website

--- routes/web.php
+++ routes/web.php
@@ -2,20 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
-    return view('welcome');
+    return view('menu');
 });
-
+Route::get('/contact', function () {
+    return view('contact');
+});
+Route::get('/news', function () {
+    return view('news');
+});
 
 
